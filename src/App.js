5c16c14dc970dfc9b850a013292eb9d419185090
notagoodnaina <92837397+notagoodnaina@users.noreply.github.com>
2023-06-27 22:31:42 +0530

    Base file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+       <Footer/>
+    </>
+  );
+}
+
+export default App;
