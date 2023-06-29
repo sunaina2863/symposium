651d9f0c4001fd822b05cbbf2a066541b4099e08
manoj_k <kfghjkl091@gmail.com>
2023-06-29 10:33:33 +0530

    footer_init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import { Route, BrowserRouter, Routes } from "react-router-dom";
+// import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "./App.css";
+import "./bootstrap-4.5.3-dist//css/bootstrap.min.css";
+import Footer from "./Home/footer";
 function App() {
   return (
     <>
-       <Footer/>
+      <Footer />
     </>
   );
 }
