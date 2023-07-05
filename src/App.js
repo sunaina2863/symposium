e3a94eb03891bbda4d0de901c4329171a20c1b24
manoj_k <kfghjkl091@gmail.com>
2023-07-05 22:53:22 +0530

    team_init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Home/navbar';
-import Home from './Home/home';
-import Events from './Events/events';
-import Footer from './Home/footer';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Home/navbar";
+import Home from "./Home/home";
+import Events from "./Events/events";
+import Footer from "./Home/footer";
+import Team from "./Team/team";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,12 +13,12 @@ function App() {
       <Navbar></Navbar>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route  path="/events"  element={<Events />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/team" element={<Team />} />
         </Routes>
       </BrowserRouter>
       <Footer></Footer>
-      
     </>
   );
 }
