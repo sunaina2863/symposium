99779d3530db0d0fec3d295a879ce7ba529a6875
notagoodnaina <92837397+notagoodnaina@users.noreply.github.com>
2023-07-11 08:13:36 +0530

    added title

--- src/Home/about_us.js
+++ src/Home/about_us.js
@@ -4,14 +4,21 @@ import "./about_us.css";
 const AboutUs = () => {
     return (
         <>
-            <div class='container py-5 px-5'>
-                <div class="container body-text px-5 py-5" style=
-                    {{width: "80%",
-                    backgroundcolor:"gray",}}
+            <div class='container py-5'>
+                <div class="container body-title px-1 " style=
+                    {{width: "40%",}}
                 >
                     <p style=
-                        {{color: "white",
-                        fontsize:"100px",}}
+                        {{color: "white",}}
+                    >
+                        ABOUT US
+                    </p>
+                </div>
+                <div class="container body-text px-5 py-5" style=
+                    {{width: "80%",}}
+                >
+                    <p style=
+                        {{color: "white",}}
                     >
                             MSES is the departmental society of Materials Science and Engineering department. Materials Science and Engineering (MSE) combines engineering, physics, chemistry, computational principles and creativity to solve problems related to biotechnology, manufacturing, information technology, nano materials and electronics. In light of this interdisciplinary tradition, most MSES events are open to all departments. Others like a visit to Qutub Minar or Materials and Art highlight our creative side, and that of materials science in general.
                     </p>
