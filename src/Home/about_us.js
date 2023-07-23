cb09f646d4db1e3f8bfa3e7589e908b999c44ca3
manoj_k <kfghjkl091@gmail.com>
2023-07-23 21:32:57 +0530

    fix_home_achie

--- src/Home/about_us.js
+++ src/Home/about_us.js
@@ -2,32 +2,28 @@ import React from "react";
 import "./about_us.css";
 
 const AboutUs = () => {
-    return (
-        <>
-            <div class='container py-5'>
-                <div class="container body-title px-1 " style=
-                    {{width: "40%",}}
-                >
-                    <p style=
-                        {{color: "white",}}
-                    >
-                        ABOUT US
-                    </p>
-                </div>
-                <div class="container body-text px-5 py-5" style=
-                    {{width: "80%",}}
-                >
-                    <p style=
-                        {{color: "white",}}
-                    >
-                            MSES is the departmental society of Materials Science and Engineering department. Materials Science and Engineering (MSE) combines engineering, physics, chemistry, computational principles and creativity to solve problems related to biotechnology, manufacturing, information technology, nano materials and electronics. In light of this interdisciplinary tradition, most MSES events are open to all departments. Others like a visit to Qutub Minar or Materials and Art highlight our creative side, and that of materials science in general.
-                    </p>
-                </div>
-               
-                    
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div class="container py-5">
+        <div class="container body-title px-1 " style={{ width: "40%" }}>
+          <p style={{ color: "white" }}>ABOUT US</p>
+        </div>
+        <div class="container body-text px-5 py-5" style={{ width: "80%" }}>
+          <p style={{ color: "white" }} className="about-us">
+            MSES is the departmental society of Materials Science and
+            Engineering department. Materials Science and Engineering (MSE)
+            combines engineering, physics, chemistry, computational principles
+            and creativity to solve problems related to biotechnology,
+            manufacturing, information technology, nano materials and
+            electronics. In light of this interdisciplinary tradition, most MSES
+            events are open to all departments. Others like a visit to Qutub
+            Minar or Materials and Art highlight our creative side, and that of
+            materials science in general.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutUs;
--- src/Home/landing.js
+++ src/Home/landing.js
@@ -1,17 +1,18 @@
 import React from "react";
-import gif from './landing.gif';
-
+import gif from "./landing.gif";
+import "./landing.css";
 const Landing = () => {
-    return(
-        <img 
-            style={{
-                
-            }}
-            src={gif} 
-            class="img-fluid" 
-            alt="image not available"
-        />
-    )
- };
+  return (
+    <>
+      <img
+        style={{}}
+        src={gif}
+        class="img-fluid landing-img"
+        alt="image not available"
+      />
+      <div className="landing-holder">Welcome Freshers 2023</div>
+    </>
+  );
+};
 
- export default Landing;
+export default Landing;
