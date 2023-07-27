bd4aebb6064d9b1609cee02c2a3ed58b98c4c1d9
Chirag <91711376+chirag-dhiman@users.noreply.github.com>
2023-07-27 20:04:31 +0530

    descriptions

--- src/Events/events.js
+++ src/Events/events.js
@@ -3,6 +3,16 @@ import "./events.css";
 import Card from "./cards";
 import png from "./events.png";
 import td1 from "./Teachers-day-1.jpeg";
+import td2 from "./Teachers-day-2";
+import mt1 from "./MaT1";
+import mt2 from "./MaT2";
+import i1 from "./intern1";
+import i2 from "./intern2";
+import b1 from "./bollywoodDay";
+import p1 from "./pitch1";
+import p2 from "./pitch2";
+import f1 from "./freshersParty1";
+import f2 from "./freshersParty2";
 import q1 from "./Qutub-1.jpeg";
 //@dev instructions for adding json attribute content
 // add at max 2 images and desctiption as a must
@@ -17,38 +27,38 @@ const cardData = [
   {
     id: 2,
     title: "Teachers Day",
-    content: [td1, td1],
+    content: [td1, td2],
     description: "Hello world this is a description. Just to entertain",
   },
   {
     id: 3,
     title: "Materials and Art",
-    content: [td1 , td1],
+    content: [mt1 , mt2],
     description: "Hello world this is a description.",
   },
   {
     id: 4,
     title: "Intern Ke Funde",
-    content: [td1, td1],
+    content: [i1, i2],
     description: "Hello world this is a description.",
   },
   {
     id: 5,
     title: "Bollywood Day",
-    content: "This is the content of card 1.",
+    content: [b1],
     description: "Hello world this is a description.",
   },
   {
     id: 6,
     title: "Pitch Perfect",
-    content: "This is the content of card 2.",
+    content: [p1,p2],
     description: "Hello world this is a description.",
   },
 
   {
     id: 7,
     title: "Freshers Party",
-    content: [],
+    content: [f1, f2],
     description: "Hello world this is a description.",
   },
 ];
