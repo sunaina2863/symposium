6d1dbf45d5f4bbd85e3d971daa0801f6dde66877
notagoodnaina <sunaina2863@gmail.com>
2023-08-29 18:53:48 +0530

    fixed Sponsors

--- src/Sponsors/sponsors.js
+++ src/Sponsors/sponsors.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./sponsors.css";
 //@dev instructions for adding json attribute content
 // add at max 2 images and desctiption as a must
-const Events = () => {
+const Sponsors = () => {
   return (
     <>
     </>
