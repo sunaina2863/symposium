665379175f5813b8037b8b8d32231cc2875735cf
notagoodnaina <sunaina2863@gmail.com>
2023-08-29 18:37:40 +0530

    deleted redundant files

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Landing from "./landing";
-import AboutUs from "./about_us"
 
 const Home = () => {
     return (
         <>
-        <Landing></Landing>
-        <AboutUs></AboutUs>
         </>
     )
 };
