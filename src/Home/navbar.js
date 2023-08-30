cc5a692b5278620f151fbe3ec71f9a782ecc4001
Shivannsh <bb1221392@iitd.ac.in>
2023-08-30 17:46:42 +0530

    Main branch pulled

--- src/Home/navbar.js
+++ src/Home/navbar.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Logo from "./MSES_Logo.svg";
 import "./navbar.css";
@@ -18,7 +19,7 @@ const Navbar = () => {
         </div>
       </label>
       <ul>
-        <li><a class="active" href="/">Home</a></li>
+        <li><a class="active" href="#">Home</a></li>
         <li><a href="/sponsors">Sponsors</a></li>
         <li><a href="/team">Team</a></li>
         
