18922605ba85e42ce1794b57d16f53331458f915
notagoodnaina <sunaina2863@gmail.com>
2023-09-02 15:04:50 +0530

    floated events

--- src/Home/eventData.js
+++ src/Home/eventData.js
@@ -1,8 +1,10 @@
+import industry from "./industry.png";
+
 export const eventData =[
     {
         "id": 1,
-        "image": "./logoblack.png",
+        "image": industry,
         "title": "Lorem Ipsum",
-        "desc": "xyz event"
+        "desc": "Calling all Aspiring Problem Solvers! Unleash your ingenuity and let your brilliance shine as you tackle real-world challenges head-on. Are you up for the challenge? It's time to turn your ideas into solutions and transform industries."
     }
 ]
