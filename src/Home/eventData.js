395f45cb5c739b57168e05961fd22f4f388951f2
notagoodnaina <sunaina2863@gmail.com>
2023-09-02 21:28:14 +0530

    added components

--- src/Home/eventData.js
+++ src/Home/eventData.js
@@ -4,7 +4,28 @@ export const eventData =[
     {
         "id": 1,
         "image": industry,
-        "title": "Lorem Ipsum",
-        "desc": "Calling all Aspiring Problem Solvers! Unleash your ingenuity and let your brilliance shine as you tackle real-world challenges head-on. Are you up for the challenge? It's time to turn your ideas into solutions and transform industries."
+        "title": "Industry Problem Solving",
+        "desc": "Calling all Aspiring Problem Solvers! Unleash your ingenuity and let your brilliance shine as you tackle real-world challenges head-on. Are you up for the challenge? It's time to turn your ideas into solutions and transform industries.",
+        "venue": "LHC123",
+        "date": "01/01/01",
+        "time": "23:59"
+    },
+    {
+        "id": 1,
+        "image": industry,
+        "title": "Industry Problem Solving",
+        "desc": "Calling all Aspiring Problem Solvers! Unleash your ingenuity and let your brilliance shine as you tackle real-world challenges head-on. Are you up for the challenge? It's time to turn your ideas into solutions and transform industries.",
+        "venue": "LHC123",
+        "date": "01/01/01",
+        "time": "23:59"
+    },
+    {
+        "id": 1,
+        "image": industry,
+        "title": "Industry Problem Solving",
+        "desc": "Calling all Aspiring Problem Solvers! Unleash your ingenuity and let your brilliance shine as you tackle real-world challenges head-on. Are you up for the challenge? It's time to turn your ideas into solutions and transform industries.",
+        "venue": "LHC123",
+        "date": "01/01/01",
+        "time": "23:59"
     }
 ]
--- src/Home/events.js
+++ src/Home/events.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import './events.css'; // Import your CSS file for styling
 import {eventData} from "./eventData";
 
 function Events() {
 
   return (
     <div className="event-gallery">
-      <h2>Event Gallery</h2>
-      <div className="event-list">
-        {eventData.map(event => (
-          <div key={event.id} className="event-card">
-            <img src={event.image} alt={event.title} className="event-image" />
-            <div className="event-details">
-              <p className="event-title">{event.desc}</p>
+      <h2>Events</h2>
+      <div className="container">   
+          <div class="row g-5 my-5 pb-5 px-5">
+          {eventData.map(event => (
+            <div class="col-6">
+              <div key={event.id} className="card">
+                <img src={event.image} alt={event.title} className="event-image" style={{}}/>
+                <div class="card-body">
+                  {/*<h5 class="card-title">{event.title}</h5>*/}
+                  <p class="card-text">{event.desc}</p>
+                  <p class="card-text"> Venue: {event.venue} </p>
+                  <p class="card-text"> Date: {event.date} </p>
+                  <p class="card-text"> Time: {event.time} </p>
+                </div>
+                <div class="card-footer">
+                  <div> <a href="#" class="btn ">Regsiter Here</a></div>
+                </div>
+              </div>
             </div>
+            ))}
           </div>
-        ))}
       </div>
     </div>
   );
