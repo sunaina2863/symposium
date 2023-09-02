2398cf2c351d476036ea7e072995ac8c58313678
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2023-09-02 22:55:04 +0530

    Update about.js

--- src/About/about.js
+++ src/About/about.js
@@ -4,7 +4,7 @@ function About() {
     return(
         <div>
         <h1 classname='page-head'>About Dravyakrit</h1>
-        <p classname='page-content'>Dravyakrit is the first edition of symposium organized by Materials Science and Engineering Society. We aim to bring together researchers, innovators, and experts to delve into the latest advancements in materials science across various domains. With an emphasis on collaboration, knowledge exchange, and groundbreaking ideas, this symposium will serve as a platform for invigorating discussions and meaningful connections</p>
+        <h2 classname='page-content'>Dravyakrit is the first edition of symposium organized by Materials Science and Engineering Society. We aim to bring together researchers, innovators, and experts to delve into the latest advancements in materials science across various domains. With an emphasis on collaboration, knowledge exchange, and groundbreaking ideas, this symposium will serve as a platform for invigorating discussions and meaningful connections</h2>
         </div>
     )
 }
