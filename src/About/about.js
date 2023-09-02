596d734c3bbf184d120c2b0f205751877d6b4f0c
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2023-09-02 23:13:45 +0530

    Update about.js

--- src/About/about.js
+++ src/About/about.js
@@ -2,9 +2,9 @@ import React from "react";
 import './about.css'
 function About() {
     return(
-        <div>
-        <h1 classname='page-head'>About Dravyakrit</h1>
-        <h2 classname='page-content'>Dravyakrit is the first edition of symposium organized by Materials Science and Engineering Society. We aim to bring together researchers, innovators, and experts to delve into the latest advancements in materials science across various domains. With an emphasis on collaboration, knowledge exchange, and groundbreaking ideas, this symposium will serve as a platform for invigorating discussions and meaningful connections</h2>
+        <div className="about">
+        <h1 className='page-head'>About Dravyakrit</h1>
+        <p className='page-content'> Dravyakrit is the first edition of symposium organized by Materials Science and Engineering Society. We aim to bring together researchers, innovators, and experts to delve into the latest advancements in materials science across various domains. With an emphasis on collaboration, knowledge exchange, and groundbreaking ideas, this symposium will serve as a platform for invigorating discussions and meaningful connections.</p>
         </div>
     )
 }
