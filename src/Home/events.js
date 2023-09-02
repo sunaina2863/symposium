19d54942a2cb1de30d582893611ce6a454ff5f6a
manoj_k <kfghjkl091@gmail.com>
2023-09-02 22:35:08 +0530

    events home responsive bg add

--- src/Home/events.js
+++ src/Home/events.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { eventData } from "./eventData";
-
+import "./events.css";
 function Events() {
   return (
     <div className="event-gallery">
       <h2>Events</h2>
-      <div className="container" style={{ background: "url(./back.png)" }}>
+      <div className="container">
         <div class="row g-5 my-5 pb-5 px-5">
           {eventData.map((event) => (
-            <div class="col-6">
+            <div class="col-md-6">
               <div key={event.id} className="card">
                 <img
                   src={event.image}
