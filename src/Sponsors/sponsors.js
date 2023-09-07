c9ff3f62f6aa30c969ef016815aabfa3916b19ff
manoj_k <kfghjkl091@gmail.com>
2023-09-07 23:02:41 +0530

    pre-host

--- src/Sponsors/sponsors.js
+++ src/Sponsors/sponsors.js
@@ -1,55 +1,25 @@
 import React from "react";
 import "./sponsors.css";
-import background from './background.svg';
-import member12 from './photo.jpeg';
+import background from "./background.svg";
+import sponsor1 from "./R.jpg";
 const Sponsors = () => {
   return (
     <>
       <div className="container-fluid main">
-        <img src={background} alt="jwef"/>
-    </div>
-        <div className="name-holder">
-          <div className="banner-title">SPONSORS</div>
-        </div>
-        <div className="sponsors-container-boss" id="team">
-
-
+        <img src={background} alt="jwef" />
+      </div>
+      <div className="name-holder">
+        <div className="banner-title">SPONSORS</div>
+      </div>
+      <div className="sponsors-container-boss" id="team">
         <div className="member-q sponsors-item">
-          <img src={member12} className="sponsors-img" alt=" jcweofw"/>
+          <img src={sponsor1} className="sponsors-img" alt=" jcweofw" />
           <div className="span">
-            <h1>Convener</h1>
-            <h2>Shivansh</h2>
+            <h1>Gold</h1>
+            <h2> xyz </h2>
           </div>
         </div>
-        <div className="member-q sponsors-item">
-          <img src={member12} className="sponsors-img" alt="ewjfn"/>
-          <div className="span">
-            <h1>Convener</h1>
-            <h2>Shivansh</h2>
-          </div>
-        </div>
-        <div className="member-q sponsors-item">
-          <img src={member12} className="sponsors-img" alt="fjkwfv"/>
-          <div className="span">
-            <h1>Convener</h1>
-            <h2>Shivansh</h2>
-          </div>
-        </div>
-        <div className="member-q sponsors-item">
-          <img src={member12} className="sponsors-img" alt="cjkewnc"/>
-          <div className="span">
-            <h1>Convener</h1>
-            <h2>Shivansh</h2>
-          </div>
-        </div>
-        <div className="member-q sponsors-item">
-          <img src={member12} className="sponsors-img" alt="cmckwenk"/>
-          <div className="span">
-            <h1>Convener</h1>
-            <h2>Shivansh</h2>
-          </div>
-        </div> 
-        </div>
+      </div>
     </>
   );
 };
