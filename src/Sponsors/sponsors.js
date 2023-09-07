ee5a89ad9508cf2c9ee3d50f4604caad2f823992
Divyansh Singh <137671160+divyansh-singh010@users.noreply.github.com>
2023-09-07 19:42:19 +0530

    new changes-collision sort

--- src/Sponsors/sponsors.js
+++ src/Sponsors/sponsors.js
@@ -11,40 +11,40 @@ const Sponsors = () => {
         <div className="name-holder">
           <div className="banner-title">SPONSORS</div>
         </div>
-        <div className="team-container-boss" id="team">
+        <div className="sponsors-container-boss" id="team">
 
 
-        <div className="member-1 flex-item">
-          <img src={member12} className="member-img" alt=" jcweofw"/>
-          <div className="name">
+        <div className="member-q sponsors-item">
+          <img src={member12} className="sponsors-img" alt=" jcweofw"/>
+          <div className="span">
             <h1>Convener</h1>
             <h2>Shivansh</h2>
           </div>
         </div>
-        <div className="member-1 flex-item">
-          <img src={member12} className="member-img" alt="ewjfn"/>
-          <div className="name">
+        <div className="member-q sponsors-item">
+          <img src={member12} className="sponsors-img" alt="ewjfn"/>
+          <div className="span">
             <h1>Convener</h1>
             <h2>Shivansh</h2>
           </div>
         </div>
-        <div className="member-1 flex-item">
-          <img src={member12} className="member-img" alt="fjkwfv"/>
-          <div className="name">
+        <div className="member-q sponsors-item">
+          <img src={member12} className="sponsors-img" alt="fjkwfv"/>
+          <div className="span">
             <h1>Convener</h1>
             <h2>Shivansh</h2>
           </div>
         </div>
-        <div className="member-1 flex-item">
-          <img src={member12} className="member-img" alt="cjkewnc"/>
-          <div className="name">
+        <div className="member-q sponsors-item">
+          <img src={member12} className="sponsors-img" alt="cjkewnc"/>
+          <div className="span">
             <h1>Convener</h1>
             <h2>Shivansh</h2>
           </div>
         </div>
-        <div className="member-1 flex-item">
-          <img src={member12} className="member-img" alt="cmckwenk"/>
-          <div className="name">
+        <div className="member-q sponsors-item">
+          <img src={member12} className="sponsors-img" alt="cmckwenk"/>
+          <div className="span">
             <h1>Convener</h1>
             <h2>Shivansh</h2>
           </div>
